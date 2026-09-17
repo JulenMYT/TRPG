391bbc3cb83f2b44479cec6dc689fac69d1a8a26
Julen <jmontes@gaming.tech>
2026-09-17 13:50:04 +0200

    delete old project

--- Source/ProjectTRPG/ProjectTRPG.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProjectTRPG.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectTRPG, "ProjectTRPG" );
